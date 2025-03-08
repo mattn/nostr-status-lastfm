8733d22849634612704bbae9ab9d0c099bbad2d9
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2025-03-08 13:54:12 +0900

    update

--- main.go
+++ main.go
@@ -5,9 +5,13 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/url"
 	"os"
+	"time"
 
 	"cloud.google.com/go/firestore"
+	"github.com/nbd-wtf/go-nostr"
+	"github.com/nbd-wtf/go-nostr/nip19"
 	"github.com/ndyakov/go-lastfm"
 	"google.golang.org/api/option"
 	"google.golang.org/grpc/codes"
@@ -19,6 +23,13 @@ const name = "nostr-status-lastfm"
 const version = "0.0.1"
 
 var revision = "HEAD"
+
+var postRelays = []string{
+	"wss://relay-jp.nostr.wirednet.jp",
+	"wss://yabu.me",
+	"wss://relay.damus.io",
+	"wss://nostr.compile-error.net",
+}
 
 func main() {
 	var lastFmApiKey string
@@ -40,6 +51,12 @@ func main() {
 		fmt.Println(version)
 		os.Exit(0)
 	}
+
+	nsec := os.Getenv("BOT_NSEC")
+	if nsec == "" {
+		log.Fatal("BOT_NSEC is required")
+	}
+
 	ctx := context.Background()
 	sa := option.WithCredentialsFile(firestoreJsonFile)
 	client, err := firestore.NewClient(ctx, firestoreProjectID, sa)
@@ -66,20 +83,66 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	var status string
 	for _, track := range resp.RecentTracks {
 		if track.NowPlaying == "" {
 			continue
 		}
-		status := fmt.Sprintf("%s - %s\n", track.Artist.Name, track.Name)
-		if status == lastStatus {
+		curr := fmt.Sprintf("%s - %s\n", track.Artist.Name, track.Name)
+		if curr == lastStatus {
 			continue
 		}
-		_, err = doc.Set(ctx, map[string]any{
-			"status": status,
-		})
-		if err != nil {
-			log.Fatalln(err)
-		}
+		status = curr
+		log.Println(status)
 		break
+	}
+
+	if status == "" || status == lastStatus {
+		return
+	}
+
+	_, err = doc.Set(ctx, map[string]any{
+		"status": status,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var sk string
+	var pub string
+	if _, s, err := nip19.Decode(nsec); err != nil {
+		log.Fatal(err)
+	} else {
+		sk = s.(string)
+	}
+	if pub, err = nostr.GetPublicKey(sk); err != nil {
+		log.Fatal(err)
+	}
+
+	var ev nostr.Event
+	ev.PubKey = pub
+	ev.Content = status
+	ev.CreatedAt = nostr.Now()
+	ev.Tags = nostr.Tags{
+		nostr.Tag{"d", "music"},
+		nostr.Tag{"expiration", fmt.Sprint(time.Now().Add(5 * time.Minute).Unix())},
+		nostr.Tag{"r", "spotify:search:" + url.QueryEscape(status)},
+	}
+	ev.Kind = nostr.KindUserStatuses
+	if err := ev.Sign(sk); err != nil {
+		log.Fatal(err)
+	}
+	for _, r := range postRelays {
+		relay, err := nostr.RelayConnect(context.Background(), r)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		err = relay.Publish(context.Background(), ev)
+		if err != nil {
+			log.Println(err)
+		}
+		relay.Close()
 	}
 }
