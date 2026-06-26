b641a73ffbf495101e8e730cea8d701974e5a23f
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-06-26 23:56:46 +0900

    require successful relay publish

--- main_test.go
+++ main_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/nbd-wtf/go-nostr"
+	"github.com/nbd-wtf/go-nostr/nip19"
+)
 
 func TestGetenv(t *testing.T) {
 	t.Setenv("DATABASE_KEY", "custom-status-key")
@@ -13,5 +18,23 @@ func TestGetenv(t *testing.T) {
 func TestGetenvFallback(t *testing.T) {
 	if got := getenv("DATABASE_KEY", "nostr-status-lastfm"); got != "nostr-status-lastfm" {
 		t.Fatalf("getenv returned %q, want %q", got, "nostr-status-lastfm")
+	}
+}
+
+func TestPublishEventFailsWithoutRelaySuccess(t *testing.T) {
+	sk := nostr.GeneratePrivateKey()
+	nsec, err := nip19.EncodePrivateKey(sk)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	oldPostRelays := postRelays
+	postRelays = nil
+	t.Cleanup(func() {
+		postRelays = oldPostRelays
+	})
+
+	if err := publishEvent(nsec, "artist - track"); err == nil {
+		t.Fatal("publishEvent returned nil, want error")
 	}
 }
